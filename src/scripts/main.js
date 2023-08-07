82fc548c32f77a09b849a2fa6d3bc8760d6e3402
Bruno Oliveira <bruno1_oliveira@hotmail.com>
2023-08-07 17:44:27 -0300

    Feat: Finalizado parcialmente secao personagens, criado lista e galeria de imagens

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,53 @@
-console.log('Testando')
+const buttons = document.querySelectorAll('[data-tab-button]');
+const tabs = document.querySelectorAll('[data-tab-id]');
+const lists = document.querySelectorAll('[data-tab-list]');
+const images = document.querySelectorAll('[data-tab-img]');
+
+document.addEventListener('DOMContentLoaded', () => {
+    for(let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', (btn) => {
+            const targetTab = btn.target.dataset.tabButton;
+            const tab = document.querySelector(`[data-tab-id=${targetTab}]`);
+            removeActiveGallery();
+            tab.classList.add('characters__gallery--is-active');
+            removeActiveButtons();
+            btn.target.classList.add('characters__tabs__button--is-active');
+        });
+    };
+
+    for(let i = 0; i < lists.length; i++) {
+        lists[i].addEventListener('click', (list) => {
+            const targetList = list.target.dataset.tabList;
+            const image = document.querySelector(`[data-tab-img=${targetList}]`);
+            removeActiveImage();
+            image.classList.add('characters__gallery__images__img--is-active');
+            removeActiveGalleryList();
+            list.target.classList.add('gallery__img--is-active');
+        });
+    };
+});
+
+// Seção galeria de personagens
+const removeActiveButtons = () => {
+    for(let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove('characters__tabs__button--is-active');
+    };
+};
+
+const removeActiveGallery = () => {
+    for(let i = 0; i < tabs.length; i++) {
+        tabs[i].classList.remove('characters__gallery--is-active');
+    };
+};
+
+const removeActiveGalleryList = () => {
+    for(let i = 0; i < lists.length; i++) {
+        lists[i].classList.remove('gallery__img--is-active');
+    };
+}
+
+const removeActiveImage = () => {
+    for(let i = 0; i < images.length; i++) {
+        images[i].classList.remove('characters__gallery__images__img--is-active');
+    };
+};
