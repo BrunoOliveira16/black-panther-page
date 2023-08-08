68b30e54f32a145f9482ef3adc8253bbb37fa281
Bruno Oliveira <bruno1_oliveira@hotmail.com>
2023-08-07 23:24:59 -0300

    :recycle: Refactor: Modificado laço for para forEach e criado metodos reutilizaveis JS para secao personagens

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,53 +1,56 @@
 const buttons = document.querySelectorAll('[data-tab-button]');
 const tabs = document.querySelectorAll('[data-tab-id]');
-const lists = document.querySelectorAll('[data-tab-list]');
+const listImages = document.querySelectorAll('[data-tab-list]');
 const images = document.querySelectorAll('[data-tab-img]');
 
 document.addEventListener('DOMContentLoaded', () => {
-    for(let i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener('click', (btn) => {
-            const targetTab = btn.target.dataset.tabButton;
-            const tab = document.querySelector(`[data-tab-id=${targetTab}]`);
-            removeActiveGallery();
-            tab.classList.add('characters__gallery--is-active');
-            removeActiveButtons();
-            btn.target.classList.add('characters__tabs__button--is-active');
-        });
-    };
+    buttons.forEach(button => {
+        button.addEventListener('click', handleCharactersButtonClick);
+    });
 
-    for(let i = 0; i < lists.length; i++) {
-        lists[i].addEventListener('click', (list) => {
-            const targetList = list.target.dataset.tabList;
-            const image = document.querySelector(`[data-tab-img=${targetList}]`);
-            removeActiveImage();
-            image.classList.add('characters__gallery__images__img--is-active');
-            removeActiveGalleryList();
-            list.target.classList.add('gallery__img--is-active');
-        });
-    };
+    listImages.forEach(list => {
+        list.addEventListener('click', handleListImagesClick);
+    });
 });
 
-// Seção galeria de personagens
-const removeActiveButtons = () => {
-    for(let i = 0; i < buttons.length; i++) {
-        buttons[i].classList.remove('characters__tabs__button--is-active');
-    };
+
+// Método para a lista de personagens
+const handleCharactersButtonClick = (btn) => {
+    const targetTab = btn.target.dataset.tabButton;
+    const tab = document.querySelector(`[data-tab-id=${targetTab}]`);
+
+    removeActiveClass(tabs, 'characters__gallery--is-active');
+    addActiveClass(tab, 'characters__gallery--is-active');
+    removeActiveClass(buttons,'characters__tabs__button--is-active');
+    addActiveClass(btn.target,'characters__tabs__button--is-active');
+
+    // Adiciona as classes ao primeiro item da galeria de imagens
+    const firstList = tab.querySelector('[data-tab-list]');
+    const firstImage = tab.querySelector('[data-tab-img]');
+    addActiveClass(firstList, 'gallery__img--is-active');
+    addActiveClass(firstImage, 'characters__gallery__images__img--is-active');
 };
 
-const removeActiveGallery = () => {
-    for(let i = 0; i < tabs.length; i++) {
-        tabs[i].classList.remove('characters__gallery--is-active');
-    };
+
+// Método para a galeria de imagens
+const handleListImagesClick = (list) => {
+    const targetListImage = list.target.dataset.tabList;
+    const image = document.querySelector(`[data-tab-img=${targetListImage}]`);
+
+    removeActiveClass(images, 'characters__gallery__images__img--is-active');
+    addActiveClass(image, 'characters__gallery__images__img--is-active');
+    removeActiveClass(listImages, 'gallery__img--is-active');
+    addActiveClass(list.target, 'gallery__img--is-active');
 };
 
-const removeActiveGalleryList = () => {
-    for(let i = 0; i < lists.length; i++) {
-        lists[i].classList.remove('gallery__img--is-active');
-    };
-}
 
-const removeActiveImage = () => {
-    for(let i = 0; i < images.length; i++) {
-        images[i].classList.remove('characters__gallery__images__img--is-active');
-    };
+// Método para remover e adicionar classes
+const removeActiveClass = (elements, className) => {
+    elements.forEach(element => {
+        element.classList.remove(className);
+    });
+};
+
+const addActiveClass = (elements, className) => {
+    elements.classList.add(className);
 };
